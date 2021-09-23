3999e6da0e69b2a01fc285c4e08e888f2b909857
balthamer <mallred.emt@gmail.com>
2021-09-23 12:54:49 -0400

    Class notes

--- SDEV-120/brute_force.py
+++ SDEV-120/brute_force.py
@@ -1,6 +1,16 @@
 import hashlib
 import string
 import itertools
+
+with open ("shadow.txt","r") as fShadow:
+    with open ("pass_list.txt","r") as fPass_list:
+        for line in fShadow:
+            for passwd in fPass_list:
+                hashed = hashlib.md5(passwd[:-1].encode("UTF-8")).hexdigest()
+                if line[:-1] == hashed:
+                    print(f"\n\nCracked\n\n{hashed}    {passwd}\n\nThe password is {passwd}.")
+                    quit()
+
 with open ("shadow.txt","r") as fShadow:
     for line in fShadow:
         for x in range(1,25):
